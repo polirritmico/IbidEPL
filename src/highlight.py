7b369e670925d04ca793237cff637c786dd65904
polirritmico <ebra187@gmail.com>
2021-05-24 22:19:51 -0400

    minor edit

--- src/highlight.py
+++ src/highlight.py
@@ -5,25 +5,26 @@
 from PyQt5.QtCore import QRegExp
 from PyQt5.QtGui import QTextCharFormat, QTextCursor, QColor
 
+
 def highlight(qtextedit, separator):
     separator_highlight = separator
-    
+
     if separator_highlight == "":
         return
-    
+
     # Definimos el color
     fmt = QTextCharFormat()
     fmt.setForeground(QColor("black"))
     fmt.setBackground(QColor("darkCyan"))
-    
+
     # Restablecemos el color
     cursor = qtextedit.textCursor()
     cursor.select(QTextCursor.Document)
     cursor.setCharFormat(QTextCharFormat())
     cursor.clearSelection()
     qtextedit.setTextCursor(cursor)
-    
-    # Regular
+
+    # RegEx
     expression = QRegExp(separator_highlight)
     qtextedit.moveCursor(QTextCursor.Start)
     cursor = qtextedit.textCursor()
