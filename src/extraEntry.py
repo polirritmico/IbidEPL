7a2f23ce0550f0c7d2c72625c911fbfe8def8f10
polirritmico <ebra187@gmail.com>
2021-05-31 04:35:41 -0400

    clean functions and var

--- src/extraEntry.py
+++ src/extraEntry.py
@@ -9,9 +9,6 @@ class ExtraEntry:
     def __init__(self, _entry, _note_ref):
         self.entry = _entry
         self.note_ref = _note_ref
-        self.continuos = False
 
-    def insertExtraEntry(self, current_note) -> str:
-        if current_note == self.note_ref:
-            return "  " + self.entry + "\n\n"
-        return ""
+    def getEntry(self) -> str:
+        return "  " + self.entry + "\n\n"
