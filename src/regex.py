61ac47405f3e753df4a429569649726f17cb0cda
polirritmico <ebra187@gmail.com>
2021-09-28 21:57:08 -0300

    added regex for TEXT pags x a y. cases

--- src/regex.py
+++ src/regex.py
@@ -25,3 +25,7 @@ REGEX_IBID = r'(?i)(ib[íi]d(em)?)[;\., (</i>)]'
 
 # Regex para analizar notas base (detecta si tienen info de pág. al final)
 REGEX_PAGE_INFO_SPLIT = r'(?i) (pp?[aá]?(?:gs|g)?(?:ina)?s?(?:\.)?(?: |(?:&nbsp;))\d+(?:-?\d*))(?:\.)?'
+
+# Regex para casos "págs. x a y."
+REGEX_RANGE_INI = r'(?i)p[aá]g(?:s)?(?:\.)? (?:\d+)'
+REGEX_RANGE_END = r'(?i)(?: )?[ya,-](?: )?(?:\d+)\.$'
